a01337e96d8f85b357d11f5653d16737705501d6
jonpas <jonpas33@gmail.com>
2019-03-11 17:11:20 +0100

    Work on UI Task 1 (UI cleanup)

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,24 +1,63 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QRandomGenerator>
+#include <QMetaEnum>
+#include <QDebug>
+
+const QPair<uint8_t, uint8_t> MainWindow::SIZE_RANGE = {4, 12};
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+    board = ui->tableWidgetBoard;
+    populateUi();
 
     toggleAlgorithmOptions(0);
     setupBoard();
 }
 
 MainWindow::~MainWindow() {
-    ui->tableWidgetBoard->clearContents();
+    for (int i = 0; i < board->rowCount(); i++) {
+        for (int j = 0; j < board->columnCount(); j++) {
+            delete board->item(i, j);
+        }
+    }
 
     delete ui;
 }
 
-void MainWindow::setupBoard() {
-    QTableWidget *board = ui->tableWidgetBoard;
+void MainWindow::populateUi() {
+    // Size
+    ui->comboBoxSize->blockSignals(true);
+    ui->comboBoxSize->clear();
+    for (uint8_t i = SIZE_RANGE.first; i <= SIZE_RANGE.second; i++) {
+        ui->comboBoxSize->addItem(QString::number(i));
+    }
+    ui->comboBoxSize->blockSignals(false);
 
-    int size = ui->comboBoxN->currentText().toInt();
-    //int algorithm = ui->comboBoxAlgorithm->currentIndex();
+    // Placement
+    ui->comboBoxPlacement->blockSignals(true);
+    ui->comboBoxPlacement->clear();
+    QMetaEnum metaPlacement = QMetaEnum::fromType<Placement>();
+    for (int i = 0; i < metaPlacement.keyCount(); i++) {
+        ui->comboBoxPlacement->addItem(prettifyCamelCase(metaPlacement.key(i)));
+    }
+    ui->comboBoxPlacement->setCurrentIndex(Placement::TopRow);
+    ui->comboBoxPlacement->blockSignals(false);
+
+    // Algorithm
+    ui->comboBoxAlgorithm->blockSignals(true);
+    ui->comboBoxAlgorithm->clear();
+    QMetaEnum metaAlgorithm = QMetaEnum::fromType<Algorithm>();
+    for (int i = 0; i < metaAlgorithm.keyCount(); i++) {
+        ui->comboBoxAlgorithm->addItem(prettifyCamelCase(metaAlgorithm.key(i)));
+    }
+    ui->comboBoxAlgorithm->blockSignals(false);
+}
+
+void MainWindow::setupBoard() {
+    int size = getBoardSize();
+    QList<QPoint> queens = getQueenPositions();
+    //Algorithm algorithm = getAlgorithm();
 
     board->setRowCount(size);
     board->setColumnCount(size);
@@ -27,14 +66,14 @@ void MainWindow::setupBoard() {
 
     QColor colorAlt(245, 222, 179); // Wheat
 
-    for (int i = 0; i < board->rowCount(); i++) {
-        for (int j = 0; j < board->columnCount(); j++) {
-            QTableWidgetItem *item = board->item(i, j);
+    for (int y = 0; y < board->rowCount(); y++) {
+        for (int x = 0; x < board->columnCount(); x++) {
+            QTableWidgetItem *item = board->item(y, x);
 
             // Create new item if it doesn't exist yet
             if (item == nullptr) {
                 item = new QTableWidgetItem();
-                board->setItem(i, j, item);
+                board->setItem(y, x, item);
 
                 // Align cell text to center
                 // TODO Queen image instead of text
@@ -42,14 +81,43 @@ void MainWindow::setupBoard() {
             }
 
             // Alternate cell color
-            if ((i + j) % 2 != 0) {
+            if ((y + x) % 2 != 0) {
                 item->setBackgroundColor(colorAlt);
             }
 
+            // Clear image
             // TODO Queen image instead of text
-            item->setText(i == 0 ? "QUEEN" : "");
+            item->setText("");
         }
     }
+
+    for (auto &queen : queens) {
+        // TODO Queen image instead of text
+        board->item(queen.x(), queen.y())->setText("QUEEN"); // X and Y swapped
+    }
+}
+
+QList<QPoint> MainWindow::getQueenPositions() {
+    int size = getBoardSize();
+    Placement type = getPlacementType();
+
+    QList<QPoint> positions;
+    if (type == Placement::TopRow) {
+        for (uint8_t i = 0; i < size; i++) {
+            positions.push_back({0, i});
+        }
+    } else if (type == Placement::Random) {
+        QRandomGenerator *gen = QRandomGenerator::global();
+        QPoint point;
+        while (positions.size() < size) {
+            point = QPoint(gen->bounded(0, size), gen->bounded(0, size));
+            if (!positions.contains(point)) {
+                positions.push_back(point);
+            }
+        }
+    }
+
+    return positions;
 }
 
 void MainWindow::toggleAlgorithmOptions(int index) {
@@ -62,7 +130,23 @@ void MainWindow::toggleAlgorithmOptions(int index) {
     ui->optionsAlgorithms->itemAt(index)->widget()->setHidden(false);
 }
 
-void MainWindow::on_comboBoxN_currentIndexChanged(const QString &/*arg1*/) {
+int MainWindow::getBoardSize() {
+    return ui->comboBoxSize->currentText().toInt();
+}
+
+MainWindow::Placement MainWindow::getPlacementType() {
+    return static_cast<Placement>(ui->comboBoxPlacement->currentIndex());
+}
+
+MainWindow::Algorithm MainWindow::getAlgorithm() {
+    return static_cast<Algorithm>(ui->comboBoxAlgorithm->currentIndex());
+}
+
+void MainWindow::on_comboBoxSize_currentIndexChanged(const QString &/*arg1*/) {
+    setupBoard();
+}
+
+void MainWindow::on_comboBoxPlacement_currentIndexChanged(const QString &/*arg1*/) {
     setupBoard();
 }
 
@@ -72,4 +156,15 @@ void MainWindow::on_pushButtonReset_clicked() {
 
 void MainWindow::on_comboBoxAlgorithm_currentIndexChanged(int index) {
     toggleAlgorithmOptions(index);
+}
+
+QString MainWindow::prettifyCamelCase(const QString &s) {
+    static QRegularExpression regExp1 {"(.)([A-Z][a-z]+)"};
+    static QRegularExpression regExp2 {"([a-z0-9])([A-Z])"};
+
+    QString result = s;
+    result.replace(regExp1, "\\1 \\2");
+    result.replace(regExp2, "\\1 \\2");
+
+    return result;
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QMainWindow>
+#include <QTableWidget>
 
 namespace Ui {
 class MainWindow;
@@ -10,18 +11,34 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
+    enum Placement { Random, TopRow };
+    Q_ENUM(Placement)
+    enum Algorithm { HillClimbing, SimulatedAnnealing, LocalBeamSearch, GeneticAlgorithm };
+    Q_ENUM(Algorithm)
+
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-private slots:
-    void on_comboBoxN_currentIndexChanged(const QString &arg1);
-    void on_pushButtonReset_clicked();
-
-    void on_comboBoxAlgorithm_currentIndexChanged(int index);
-
 private:
     Ui::MainWindow *ui;
+    QTableWidget *board;
 
+    static const QPair<uint8_t, uint8_t> SIZE_RANGE;
+
+    void populateUi();
     void setupBoard();
+    QList<QPoint> getQueenPositions();
     void toggleAlgorithmOptions(int index);
+
+    int getBoardSize();
+    Placement getPlacementType();
+    Algorithm getAlgorithm();
+
+    QString prettifyCamelCase(const QString &s);
+
+private slots:
+    void on_comboBoxSize_currentIndexChanged(const QString &arg1);
+    void on_comboBoxPlacement_currentIndexChanged(const QString &arg1);
+    void on_pushButtonReset_clicked();
+    void on_comboBoxAlgorithm_currentIndexChanged(int index);
 };
