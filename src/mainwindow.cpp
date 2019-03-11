f56f41f63d4e268417ed4da2e5af5378af24936e
jonpas <jonpas33@gmail.com>
2019-03-11 01:59:27 +0100

    Work on UI Task 1 (UI)

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -3,8 +3,47 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
+
+    setupBoard();
 }
 
 MainWindow::~MainWindow() {
+    ui->tableWidgetBoard->clearContents();
+
     delete ui;
+}
+
+void MainWindow::setupBoard() {
+    QTableWidget *board = ui->tableWidgetBoard;
+
+    int size = ui->comboBoxN->currentText().toInt();
+    //int algorithm = ui->comboBoxAlgorithm->currentIndex();
+
+    board->setRowCount(size);
+    board->setColumnCount(size);
+    board->horizontalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+    board->verticalHeader()->setSectionResizeMode(QHeaderView::Stretch);
+
+    QColor colorAlt(245, 222, 179); // Wheat
+
+    for (int i = 0; i < board->rowCount(); i++) {
+        for (int j = 0; j < board->columnCount(); j++) {
+            QTableWidgetItem *item = board->item(i, j);
+
+            // Create new item if it doesn't exist yet
+            if (item == nullptr) {
+                item = new QTableWidgetItem();
+                board->setItem(i, j, item);
+            }
+
+            // Alternate cell color
+            if ((i + j) % 2 != 0) {
+                item->setBackgroundColor(colorAlt);
+            }
+        }
+    }
+}
+
+void MainWindow::on_comboBoxN_currentIndexChanged(const QString &/*arg1*/) {
+    setupBoard();
 }
--- src/mainwindow.h
+++ src/mainwindow.h
@@ -13,6 +13,11 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    void setupBoard();
+
+private slots:
+    void on_comboBoxN_currentIndexChanged(const QString &arg1);
+
 private:
     Ui::MainWindow *ui;
 };
